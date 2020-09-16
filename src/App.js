872e2defc2a8af25b126585118c482a6a6babe8b
Raunak <sharmaraunak338@gmail.com>
2020-09-16 17:28:25 +0530

    created news app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React ,{ useState, useEffect } from 'react';
 
-function App() {
+
+export const App = () =>{
+
+  const [ news,setNews ] = useState([]);
+  const [ searchQuery,setSearchQuery] = useState("react");
+  const [ url, setURL ] = useState('http://hn.algolia.com/api/v1/search?query=react')
+
+  const [ loading, setLoading ] = useState(false);
+
+
+  const fetchNews = () =>{
+
+    //setLoading True
+    setLoading(true);
+
+    fetch(url)
+    .then(result => result.json())
+    .then(data => (setNews(data.hits),setLoading(false)))
+    .catch(error => console.log(error))
+  }
+
+  useEffect( () =>{
+    fetchNews();
+  },[url])
+
+  const handleChange = (event) =>{
+    setSearchQuery(event.target.value);
+  }
+
+  const handleSubmit = e =>{
+    e.preventDefault();
+    setURL(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`);
+
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading 
+      ? <h2>Loading......</h2>
+      : <div>
+          <h2>News</h2>
+          <form onSubmit={handleSubmit}>
+            <input type="text" name="" id="" placeholder="Search topic" value={searchQuery} onChange={handleChange}/>
+            <button type="submit">Search</button>
+          </form>
+          { news.map( (n,i) => (
+            <p key={i}>{n.title}</p>
+          ))}
+      </div>
+      }
+      
     </div>
   );
-}
 
-export default App;
+
+}
